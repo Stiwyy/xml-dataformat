2309ab5ade1b3c7455212f5cf6f4e0edc376da86
Andrin <andrinrenggli@outlook.com>
2025-03-04 11:39:21 +0100

    Aufgabe 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            DocumentBuilderFactory dcb = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = dcb.newDocumentBuilder();
+            Document document = documentBuilder.parse("src/main/resources/files/adressen.xml");
+            Node rootNode = document.getDocumentElement();
+            NodeList nodeList = rootNode.getChildNodes();
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                System.out.println(node.getNodeName());
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
